5088d721426680390d4b7a4bf02af6bd83fa347e
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2023-08-02 18:23:14 -0600

    Update Viterby.py

--- Emisor/Viterby.py
+++ Emisor/Viterby.py
@@ -1,2 +1,19 @@
 import numpy as np
 
+
+def encodificador(trama):
+    #Variables
+    G1 = [1,1,1]
+    G1 = [1,0,1]
+    
+    
+
+def main():
+    codigo = input("Ingrese la trama en binario:")
+    codigo_codificado= encodificador(codigo)
+    
+    print("Mensaje codificado")
+    print(codigo_codificado)
+    
+if __name__ == "__main__":
+    main()
