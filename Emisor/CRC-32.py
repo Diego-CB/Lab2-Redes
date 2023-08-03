00686d90d2981a00ad431cc9800de62ebe6a44de
Andrea Lam <70552370+andreadelou@users.noreply.github.com>
2023-08-03 13:15:12 -0600

    CRC32

---
+++ Emisor/CRC-32.py
@@ -0,0 +1,49 @@
+import crcmod.predefined
+
+def CRC32(trama):
+    # Polinomio estándar para CRC-32
+    polinomio = "crc-32"
+
+    #aqui se usa la funcion de la biblioteca que ayuda a con el polinomio se calcula para obtener el CRC
+    crc_func = crcmod.predefined.Crc(polinomio)
+
+    #Se pasa la trama a bytes
+    trama_bytes = bytes(int(trama[i:i+8], 2) for i in range(0, len(trama), 8))
+
+    #Se calcula el CRC-32 de la trama de bytes
+    crc_final = crc_func.new(trama_bytes).digest()
+
+    # Convierte el CRC-32 en una cadena binaria de 32 bits
+    crc_bin = bin(int.from_bytes(crc_final, byteorder='big'))[2:].zfill(32)
+
+    return crc_bin
+
+def simulacion(trama):
+    crc_temporal = trama[-32:]  #Obtiene los últimos 32 bits que son el CRC-32 recibido
+    trama_sin_crc = trama[:-32]  #Obtiene la trama original sin el CRC-32 recibido
+
+    # Calcular el CRC-32 de la trama recibida (sin el CRC-32)
+    crc_calculado_recibido = CRC32(trama_sin_crc)
+
+    if crc_calculado_recibido == crc_temporal:
+        print("La trama no contiene errores.")
+    else:
+        print("Se detectaron errores en la trama.")
+    
+def main():
+    trama = input("Ingrese la trama en binario: ")
+
+    # Calcula el CRC-32 de la trama original
+    crc_calculado = CRC32(trama)
+
+    # Concatena el CRC-32 calculado con la trama original
+    trama_con_crc = trama + crc_calculado
+
+    print("Trama con CRC-32:", trama_con_crc)
+
+    # Simulando la recepción de la trama y verificación de errores
+    simulacion(trama)
+    
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 Andrea Lam 20102
 Diego Cordova 20
 
+Para poder utilizar el lab se necesita descargar estas librerias:
+pip install crcmod
+
+
 Es importante recordar que para el codigo de Hamming agrega los bits de paridad en posiciones que son potencias de 2 (1, 2, 4, 8, etc.), y utiliza el resto de bits para la información original. Si la trama de entrada no tiene una longitud adecuada para el algoritmo de Hamming, el código podría no funcionar correctamente.
